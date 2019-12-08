9c3b5070539b5247a157b994a4eb60f5a1d35dc5
tlh22 <thancock@mhtc.co.uk>
2019-12-08 22:10:48 +0100

    Initial commit

---
+++ importMatchLists.py
@@ -0,0 +1,21 @@
+#-----------------------------------------------------------
+# Licensed under the terms of GNU GPL 2
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#---------------------------------------------------------------------
+# Tim Hancock 2019
+
+class matchLists(object):
+
+    baysMatchList = [
+        ("road_name", "RoadName"),
+        ("type", "WBayType1"),
+        ("bay_type", "WBayType2"),
+        ("category", "Category"),
+        ("hours_of_o", "WTimePeriod"),
+        ("partial_fu", "WGeometryType"),
+        ("comment_", "WNotes")
+    ]
